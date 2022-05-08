af5145b76cc6c1de1645341a3b05d4e603bae14a
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2022-05-08 21:48:38 +0530

    Added boiler plate

---
+++ app.py
@@ -0,0 +1,25 @@
+# https://github.com/ifrankandrade/ml-web-app/blob/main/deploy-lr-project/app.py
+# https://towardsdatascience.com/how-to-easily-build-your-first-machine-learning-web-app-in-python-c3d6c0f0a01c#e0d6
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    rooms = int(request.form['rooms'])
+    distance = int(request.form['distance'])
+    prediction = model.predict([[rooms, distance]])
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text=f'A house with {rooms} rooms per dwelling and located {distance} km to employment centers has a value of ${output}K')
+
+
+if __name__ == "__main__":
+    app.run()
