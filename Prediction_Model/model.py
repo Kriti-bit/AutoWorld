c3d07044097422800613123cc29a072b339a86d0
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2022-05-11 15:51:52 +0530

    Corrected routing of prediction

--- Prediction_Model/model.py
+++ Prediction_Model/model.py
@@ -1,8 +1,24 @@
+# from sklearn import linear_model
+# import pandas as pd
+# import pickle
+
+# df = pd.read_csv('cleaned_automobile_data.csv')
+
+# y = df['price']  # dependent variable
+# X = df[['num-of-doors', 'horsepower']]  # independent variable
+
+# lm = linear_model.LinearRegression()
+# lm.fit(X, y)  # fitting the model
+# pickle.dump(lm, open('model.pkl', 'wb'))  # save the model
+
+# print(lm.predict([[15, 61]]))  # format of input
+# print(f'score: {lm.score(X, y)}')
+
 from sklearn import linear_model
 import pandas as pd
 import pickle
 
-df = pd.read_csv('cleaned_automobile_data.csv')
+df = pd.read_csv('prices.csv')
 
 y = df['Value']  # dependent variable
 X = df[['Rooms', 'Distance']]  # independent variable
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # https://github.com/ifrankandrade/ml-web-app/blob/main/deploy-lr-project/app.py
 # https://towardsdatascience.com/how-to-easily-build-your-first-machine-learning-web-app-in-python-c3d6c0f0a01c#e0d6
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import pickle
 
 app = Flask(__name__)
@@ -12,13 +12,24 @@ def hello():
     return render_template('index.html')
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/analysis")
+def hello1():
+    return render_template('index.html')
+
+
+@app.route("/tryPredict")
+def tryPredict():
+    return render_template('tryPredict.html')
+
+
+@app.route("/predict",  methods=['POST'])
 def predict():
-    rooms = int(request.form['rooms'])
-    distance = int(request.form['distance'])
+    # if request.method == 'POST':
+    rooms = request.form['rooms']
+    distance = request.form['distance']
     prediction = model.predict([[rooms, distance]])
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text=f'A house with {rooms} rooms per dwelling and located {distance} km to employment centers has a value of ${output}K')
+    return render_template('predictions.html', prediction_text=f'A house with {rooms} rooms per dwelling and located {distance} km to employment centers has a value of ${output}K')
 
 
 if __name__ == "__main__":
